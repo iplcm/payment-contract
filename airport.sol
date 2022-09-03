5b3b1ec42dbac8edcbc8e4149af360d7ea149397
干净 <54979375+du5@users.noreply.github.com>
2022-09-03 18:14:31 +0800

    完善合约

--- airport.sol
+++ airport.sol
@@ -9,37 +9,31 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract PayGateway is Ownable {
     using SafeERC20 for IERC20;
 
-    address payable public to;
-
     address public immutable koge;
 
-    function changeTo(address payable _to) public onlyOwner {
-        to = _to;
-    }
-
-    constructor(address payable _to, address _koge) {
-        to = _to;
+    constructor(address _koge, address _owner) {
         koge = _koge;
+        transferOwnership(_owner);
     }
 
     function withdrawErc20(address token) public onlyOwner {
-        IERC20(token).safeTransfer(to, IERC20(token).balanceOf(address(this)));
+        IERC20(token).safeTransfer(
+            this.owner(),
+            IERC20(token).balanceOf(address(this))
+        );
     }
 
-    event evPayOrderErc20(uint256 indexed telegram, uint256 amount);
+    event evPayOrderErc20(string indexed uuid, uint256 amount);
 
-    function payOrderErc20(uint64 telegram, uint256 amount) public {
+    function payOrderErc20(string memory uuid, uint256 amount) public {
         address _sender = msg.sender;
-        IERC20(koge).safeTransferFrom(_sender, to, amount);
-        emit evPayOrderErc20(telegram, amount);
+        IERC20(koge).safeTransferFrom(_sender, this.owner(), amount);
+        emit evPayOrderErc20(uuid, amount);
     }
 
     fallback() external payable {}
 
-    event evReceiveEth(uint256 amount);
-
     receive() external payable {
-        to.transfer(msg.value);
-        emit evReceiveEth(msg.value);
+        require(false, "dping.eth pls");
     }
 }
